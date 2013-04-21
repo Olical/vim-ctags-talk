070b6d21ae9d42f36b5cdea7fe7425052cc85ed3
Oliver Caldwell <olliec87@gmail.com>
2013-04-21 10:47:05 +0100

    Finished the boilerplate.
    The app now loads and I can begin the markup.

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+Reveal.initialize({
+	controls: true,
+	progress: true,
+	history: false,
+	keyboard: true,
+	overview: true,
+	center: true,
+	loop: false,
+	rtl: false,
+	autoSlide: 0,
+	mouseWheel: false,
+	rollingLinks: true,
+	transition: 'concave',
+	transitionSpeed: 'default'
+});
